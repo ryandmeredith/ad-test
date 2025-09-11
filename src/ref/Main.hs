30416294c117d0ad255435c7aafd63bcbc1e62f2
Ryan Meredith <ryan@ryanmeredith.com>
2025-09-11 19:14:05 -0400

    Better fib

--- src/ref/Main.hs
+++ src/ref/Main.hs
@@ -6,9 +6,7 @@ import Numeric.AD (grad')
 import System.Environment (getArgs)
 
 fib :: (Ord a, Num a) => a -> a
-fib n
-  | n <= 1 = n
-  | otherwise = fib (n - 1) + fib (n - 2)
+fib n = if n <= 1 then n else fib (n - 1) + fib (n - 2)
 
 main :: IO ()
 main = do
