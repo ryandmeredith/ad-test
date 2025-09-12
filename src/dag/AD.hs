de0a528c1a7cebedebbfd84a237aebd83779dd79
Ryan Meredith <ryan@ryanmeredith.com>
2025-09-12 14:29:58 -0400

    Switched to pure expressions

--- src/dag/AD.hs
+++ src/dag/AD.hs
@@ -8,33 +8,32 @@ import Data.Map
 import Language.Haskell.TH
 import Prelude hiding (lookup)
 
-data BiMap = MkB (Map Name ExpQ) (Map Exp Name)
+data BiMap = MkB (Map Name Exp) (Map Exp Name)
 
 newtype D = MkD (StateT BiMap Q Name)
 
-hashcons :: ExpQ -> StateT BiMap Q Name
+hashcons :: Exp -> StateT BiMap Q Name
 hashcons e = do
   MkB m1 m2 <- get
-  e' <- lift e
-  case lookup e' m2 of
+  case lookup e m2 of
     Just n -> pure n
     Nothing -> do
       n <- lift $ newName "x"
       let m1' = insert n e m1
-          m2' = insert e' n m2
+          m2' = insert e n m2
       put $ MkB m1' m2'
       pure n
 
 lift1 :: Name -> D -> D
 lift1 f (MkD xs) = MkD $ do
   x <- xs
-  hashcons $ appE (varE f) (varE x)
+  hashcons $ AppE (VarE f) (VarE x)
 
 lift2 :: Name -> D -> D -> D
 lift2 f (MkD xs) (MkD ys) = MkD $ do
   x <- xs
   y <- ys
-  hashcons $ uInfixE (varE x) (varE f) (varE y)
+  hashcons $ UInfixE (VarE x) (VarE f) (VarE y)
 
 instance Num D where
   (+) = lift2 '(+)
@@ -43,12 +42,12 @@ instance Num D where
   negate = lift1 'negate
   abs = lift1 'abs
   signum = lift1 'signum
-  fromInteger = MkD . hashcons . litE . integerL
+  fromInteger = MkD . hashcons . LitE . IntegerL
 
 autodiff :: (D -> D) -> ExpQ
 autodiff f = do
   x <- newName "x"
   let MkD s = f $ MkD $ pure x
   (y, MkB m _) <- runStateT s $ MkB empty empty
-  let decs = foldrWithKey (\n e xs -> valD (varP n) (normalB e) [] : xs) [] m
-  lam1E (varP x) $ letE decs $ varE y
+  let decs = foldrWithKey (\n e xs -> ValD (VarP n) (NormalB e) [] : xs) [] m
+  pure $ LamE [VarP x] $ LetE decs $ VarE y
