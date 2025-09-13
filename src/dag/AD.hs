b8b45b903b709d0cc9b0c3d2226173e141eaa0a4
Ryan Meredith <ryan@ryanmeredith.com>
2025-09-13 19:59:59 -0400

    Easier derivative definitions

--- src/dag/AD.hs
+++ src/dag/AD.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LexicalNegation #-}
 {-# LANGUAGE TemplateHaskellQuotes #-}
 
 module AD (D, autodiff) where
@@ -9,7 +10,7 @@ import Data.STRef (modifySTRef', newSTRef, readSTRef, writeSTRef)
 import Language.Haskell.TH (
   Body (NormalB),
   Dec (ValD),
-  Exp (AppE, AppTypeE, DoE, InfixE, LamE, LitE, TupE, UInfixE, VarE),
+  Exp (AppE, AppTypeE, DoE, LamE, LitE, TupE, UInfixE, VarE),
   ExpQ,
   Lit (IntegerL),
   Name,
@@ -18,6 +19,7 @@ import Language.Haskell.TH (
   Stmt (BindS, LetS, NoBindS),
   Type (VarT),
   newName,
+  varE,
  )
 import Prelude hiding (lookup)
 
@@ -41,46 +43,36 @@ hashcons e back = do
       put $ MkB (f . (<>) f') ((back n' <>) . b) m'
       pure (n, n')
 
-lift1 :: Name -> (Exp -> Exp -> Exp) -> D -> D
+lift1 :: Name -> (ExpQ -> ExpQ -> ExpQ) -> D -> D
 lift1 f f' (MkD xm) = MkD $ do
   (x, x') <- xm
   y' <- lift $ newName "y'"
+  deriv <- lift $ f' (varE x) (varE y')
   hashcons (AppE (VarE f) $ VarE x) $ \n' ->
     [ BindS (VarP y') $ AppE (VarE 'readSTRef) $ VarE n'
-    , NoBindS $ AppE (AppE (VarE 'modifySTRef') $ VarE x') $ f' (VarE x) (VarE y')
+    , NoBindS $ AppE (AppE (VarE 'modifySTRef') $ VarE x') deriv
     ]
 
-lift2 :: Name -> (Exp -> Exp -> Exp -> Exp) -> (Exp -> Exp -> Exp -> Exp) -> D -> D -> D
+lift2 :: Name -> (ExpQ -> ExpQ -> ExpQ -> ExpQ) -> (ExpQ -> ExpQ -> ExpQ -> ExpQ) -> D -> D -> D
 lift2 f f1' f2' (MkD xm) (MkD ym) = MkD $ do
   (x, x') <- xm
   (y, y') <- ym
   z' <- lift $ newName "z'"
+  derivX <- lift $ f1' (varE x) (varE y) (varE z')
+  derivY <- lift $ f2' (varE x) (varE y) (varE z')
   hashcons (UInfixE (VarE x) (VarE f) (VarE y)) $ \n' ->
     [ BindS (VarP z') $ AppE (VarE 'readSTRef) $ VarE n'
-    , NoBindS $ AppE (AppE (VarE 'modifySTRef') $ VarE x') $ f1' (VarE x) (VarE y) (VarE z')
-    , NoBindS $ AppE (AppE (VarE 'modifySTRef') $ VarE y') $ f2' (VarE x) (VarE y) (VarE z')
+    , NoBindS $ AppE (AppE (VarE 'modifySTRef') $ VarE x') derivX
+    , NoBindS $ AppE (AppE (VarE 'modifySTRef') $ VarE y') derivY
     ]
 
 instance Num D where
-  (+) =
-    lift2
-      '(+)
-      (\_ _ z' -> InfixE Nothing (VarE '(+)) $ Just z')
-      (\_ _ z' -> InfixE Nothing (VarE '(+)) $ Just z')
-  (-) =
-    lift2
-      '(-)
-      (\_ _ z' -> InfixE Nothing (VarE '(+)) $ Just z')
-      (\_ _ z' -> InfixE Nothing (VarE '(-)) $ Just z')
-  (*) =
-    lift2
-      '(*)
-      (\_ y z' -> InfixE Nothing (VarE '(+)) $ Just $ UInfixE z' (VarE '(*)) y)
-      (\x _ z' -> InfixE Nothing (VarE '(+)) $ Just $ UInfixE z' (VarE '(*)) x)
-  negate = lift1 'negate $ \_ y' -> InfixE Nothing (VarE '(-)) $ Just y'
-  abs = lift1 'abs $ \x y' ->
-    InfixE Nothing (VarE '(+)) $ Just $ UInfixE y' (VarE '(*)) $ AppE (VarE 'signum) x
-  signum (MkD xs) = MkD $ xs >>= (`hashcons` const []) . AppE (VarE 'signum) . VarE . fst
+  (+) = lift2 '(+) (\_ _ z' -> [|(+ $z')|]) (\_ _ z' -> [|(+ $z')|])
+  (-) = lift2 '(-) (\_ _ z' -> [|(+ $z')|]) (\_ _ z' -> [|(- $z')|])
+  (*) = lift2 '(*) (\_ y z' -> [|(+ $z' * $y)|]) (\x _ z' -> [|(+ $z' * $x)|])
+  negate = lift1 'negate $ \_ y' -> [|(- $y')|]
+  abs = lift1 'abs $ \x y' -> [|(+ $y' * signum $x)|]
+  signum (MkD xm) = MkD $ xm >>= (`hashcons` const []) . AppE (VarE 'signum) . VarE . fst
   fromInteger n = MkD $ hashcons (LitE $ IntegerL n) $ const []
 
 autodiff :: (D -> D) -> ExpQ
